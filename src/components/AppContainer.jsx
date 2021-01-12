b9bdb09ae21fda17baf351e9394231e3777dceec
ercumentlacin <forever.ducale@gmail.com>
2021-01-12 18:09:12 +0300

    1 file modified, 1 added

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import Http404 from '../Pages/Http404'
 import ThemeContext from "../context/ThemeContext";
 import Search from "./Search/Search";
 import PopularMovies from "../Pages/PopularMovies";
@@ -29,9 +29,13 @@ const AppContainer = () => {
           <Route path="/popular">
             <PopularMovies />
           </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Search />
           </Route>
+          <Route path="/404">
+              <Http404 />
+            </Route>
+            <Redirect from="*" to="/404" />
         </Switch>
       </Router>
     </main>
